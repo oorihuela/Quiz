5a557955818c5a2db7ff149b6a89b006092fe8a8
oorihuela <oorihuelas@yahoo.es>
2015-07-30 17:22:23 +0200

    Crear preguntas

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,8 @@ router.param('quizId', quiz_controller.load); // Autoload :quizId
 router.get('/quizes', quiz_controller.index);
 router.get('/quizes/:quizId(\\d+)', quiz_controller.show);
 router.get('/quizes/:quizId(\\d+)/answer', quiz_controller.answer);
+router.get('/quizes/new', quiz_controller.new);
+router.get('/quizes/create', quiz_controller.create);
 
 /* Creditos */
 router.get('/author', function (req, res) {
