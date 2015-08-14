ac7b655936afa3219da21bed13fb083c5662befc
oorihuela <oorihuelas@yahoo.es>
2015-08-14 17:21:33 +0200

    Ejercicio P2P obligatorio Modulo 7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -36,9 +36,20 @@ exports.load = function (req, res, next, quizId) {
 /* GET quizes */
 
 exports.index = function (req, res) {
-    models.Quiz.findAll().then(function (quizes) {
-        res.render('quizes/index', { quizes: quizes, errors: [] });
-    }).catch(function(error) { next(error); });
+    if(req.query.search!="") {
+
+        var criterio = (req.query.search).replace(" ","%");
+
+        models.Quiz.findAll( { where:['pregunta like ?','%'+criterio+'%'], order:'pregunta ASC'}).then(function (quizes) {
+             res.render('quizes/index',{ quizes:quizes, errors: [] });
+        }).catch(function(error) {next(error); });
+
+    } else{ 
+
+        models.Quiz.findAll().then(function (quizes) {
+            res.render('quizes/index', { quizes: quizes, errors: [] });
+        }).catch(function(error) { next(error); });
+    }
 };
 
 
