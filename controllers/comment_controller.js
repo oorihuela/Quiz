182cbb75038a90b9b3023f8de0f1209efd967291
oorihuela <oorihuelas@yahoo.es>
2015-08-16 13:15:38 +0200

    Correcciones: Delete oncascade de comentarios, charset UTF-8, solucion mensaje Not Found al publicar un comentario

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -26,7 +26,7 @@ exports.new = function (req, res) {
 exports.create = function (req, res) {
     var comment = models.Comment.build(
         { texto: req.body.comment.texto, 
-          QuizId: req.params.quizId
+          quizid: req.params.quizId
         });
 
     comment
